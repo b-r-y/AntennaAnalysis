7555d4a56878173a620f5b526f0236dd4b7b1e9b
John Doe <johndoe@example.com>
2024-10-28 22:48:25 +0100

    fix docs build on linux

--- buildall.py
+++ buildall.py
@@ -4,8 +4,10 @@ import sys
 import os
 
 if sys.platform == "win32":
+    env_keyword = "set"
     os.system("python -m pip install -r requirements.txt")
 elif sys.platform == "linux":
+    env_keyword = "export"
     os.system("python3 -m pip install -r requirements.txt")
 
 WORKING_DIR = os.getcwd()
@@ -15,16 +17,16 @@ WORKING_DIR = os.getcwd()
 os.chdir("docs")
 
 # Create Autodoc API files
-os.system(f"set PYTHONPATH={WORKING_DIR} & sphinx-apidoc -f -e -a -T -o ./source/modules ../antenna_analysis")
+os.system(f"{env_keyword} PYTHONPATH={WORKING_DIR} && sphinx-apidoc -f -e -a -T -o ./source/modules ../antenna_analysis")
 
 # Create the covertrage reports
-os.system(f"set PYTHONPATH={WORKING_DIR} & sphinx-build -M coverage ./source ./source")
+os.system(f"{env_keyword} PYTHONPATH={WORKING_DIR} && sphinx-build -M coverage ./source ./source")
 
 # Build the HTLM Documentation
-os.system(f"set PYTHONPATH={WORKING_DIR} & sphinx-build -M html ./source ./build")
+os.system(f"{env_keyword} PYTHONPATH={WORKING_DIR} && sphinx-build -M html ./source ./build")
 
 # Build the Latex Documentation
-os.system(f"set PYTHONPATH={WORKING_DIR} & sphinx-build -M latex ./source ./build")
+os.system(f"{env_keyword} PYTHONPATH={WORKING_DIR} && sphinx-build -M latex ./source ./build")
 
 os.chdir("build/latex")
 os.system("pdflatex AntennaAnalysis.tex")
